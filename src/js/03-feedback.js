f7764ca437ab5d1053c98b8c7e40b678f26c4c4b
AnnaFesenko7 <annafecenko7@gmail.com>
2022-02-06 13:04:25 +0200

    overwritten the object in the task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from'lodash.throttle'
 const formRef = document.querySelector('.feedback-form')
 const emailRef = formRef.querySelector('input');
 const messageRef = formRef.querySelector('textarea');
-const formData = {}
+let formData = {}
 const STORAGE_KEY = 'feedback-form-state';
 formRef.addEventListener('input', throttle(onFormInput, 500));
 formRef.addEventListener('submit', onFormSubm)
@@ -20,10 +20,12 @@ function savedData() {
         emailRef.value = parseFormData.email || "";
         messageRef.value = parseFormData.message || "";
         
+        formData = parseFormData
         }
     }
 
 function onFormInput(e) {
+    
     formData[e.target.name] = e.target.value;
     console.log(" formData", formData);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
